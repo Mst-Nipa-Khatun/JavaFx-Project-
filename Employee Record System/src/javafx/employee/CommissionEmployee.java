6bf1fc770c1a2f4a0757cfb258cee4a7fa6c3ded
Mst-Nipa-Khatun <sheikhnipa01@gmail.com>
2021-03-12 00:07:58 +0600

    My First JavaFx Project

---
+++ Employee Record System/src/javafx/employee/CommissionEmployee.java
@@ -0,0 +1,41 @@
+package javafx.employee;
+
+public class CommissionEmployee extends EmployeeType {
+
+    double commission;
+    double sale;
+
+    public CommissionEmployee(String name, String id, String designation, double commission, double sale) {
+        super(name, id, designation);
+        this.commission = commission;
+        this.sale = sale;
+    }
+
+    double getSalary(double sale) {
+
+        return commission * sale;
+    }
+
+    @Override
+    double getSalary() {
+
+        return commission * sale;
+    }
+
+    @Override
+    void increaseSalary(double amt) {
+        commission += amt;
+    }
+
+    @Override
+    String display() {
+        String s = "Name: " + getName() + " ID: " + getId() + " Designation: " + getDesignation() + " Salary: " + this.getSalary() + " (Commission Employee)";
+        return s;
+    }
+
+    @Override
+    String display2() {
+        String s = "ID: " + getId() + "\nSalary: " + this.getSalary() + "\nCommission: " + commission + "\nSale: " + sale;
+        return s;
+    }
+}
---
+++ Employee Record System/src/javafx/employee/HourlyEmployee.java
@@ -0,0 +1,42 @@
+package javafx.employee;
+
+public class HourlyEmployee extends EmployeeType {
+
+    double hourlyRate;
+    int hourWorked;
+
+    public HourlyEmployee(String name, String id, String designation, double hourlyRate, int hourWorked) {
+        super(name, id, designation);
+        this.hourlyRate = hourlyRate;
+        this.hourWorked = hourWorked;
+    }
+
+    double getSalary(int hWorked) {
+
+        return hourlyRate * hWorked;
+    }
+
+    @Override
+    void increaseSalary(double amt) {
+
+        hourlyRate += amt;
+    }
+
+    @Override
+    double getSalary() {
+
+        return hourlyRate * hourWorked;
+    }
+
+    @Override
+    String display() {
+        String s = "Name: " + getName() + " ID: " + getId() + " Designation: " + getDesignation() + " Salary: " + this.getSalary() + " (Hourly Employee)";
+        return s;
+    }
+    
+    @Override
+    String display2() {
+        String s = "ID: " + getId()+ "\nSalary: " + this.getSalary() + "\nHourlyRate: "+hourlyRate+"\nHourWorked: "+hourWorked;
+        return s;
+    }
+}
